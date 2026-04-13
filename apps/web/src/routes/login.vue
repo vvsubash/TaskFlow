b83d931b70ce927f3231e25cbecae1bdd8530273
vvsubashshareplay <venkat@shareplay.tv>
2026-04-13 23:50:23 +0530

    feat: testing api in production

--- apps/web/src/routes/login.vue
+++ apps/web/src/routes/login.vue
@@ -1,7 +1,8 @@
 <template>
   <h1>Login</h1>
   <Link to="/tasks/9">9</Link>
+  <Link to="test-status">Test Status</Link>
 </template>
 <script setup lang="ts">
-import { RouterLink as Link } from 'vue-router'
+import { RouterLink as Link } from "vue-router";
 </script>
