ca58d0f5f38367bf4964fb16b8f57ba8e7b43310
vvsubashshareplay <venkat@shareplay.tv>
2026-04-13 13:46:33 +0530

    feat: adding router

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,8 +1,34 @@
 import { createRouter, createWebHistory } from 'vue-router'
-
+import Login from '@/routes/login.vue'
+import Home from '@/routes/index.vue'
+import NewTask from '@/routes/tasks/new.vue'
+import Task from '@/routes/tasks/[id].vue'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  routes: [
+    {
+      path: '/',
+      // component: Home,
+      redirect: '/login',
+    },
+    {
+      path: '/login',
+      component: Login,
+    },
+    {
+      path: '/tasks',
+      children: [
+        {
+          path: 'new',
+          component: NewTask,
+        },
+        {
+          path: ':id',
+          component: Task,
+        },
+      ],
+    },
+  ],
 })
 
 export default router
